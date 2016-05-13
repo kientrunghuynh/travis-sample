16d5c349da44416828fd9a842aa83faae53243d5
Trung Huynh <thy@tapptic.com>
2016-05-13 17:08:42 +0700

    - updated source

--- index.php
+++ index.php
@@ -3,7 +3,31 @@
 $text = 'sample';
 
 if ($text === '') {
-  echo 'false';
+    echo 'false';
 } else {
-  echo ' true';
+    
+    echo ' true';
+}
+
+$var = 'tmp';
+if ($var == 'tmp') {
+    
+    $var2 = 'tmp2';
+    
+    if ($var2 != 'tmp') {
+        if ($var2 == $var) {
+            $var3 = 'sample';
+            if ($var == $var3) 
+            {
+                $test = 'checked';
+            } else {
+                $var == $var3;
+            }
+        } 
+        
+        else {
+            $var2 = $var;
+        }
+    }
+    
 }
